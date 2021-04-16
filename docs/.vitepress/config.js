102227889d5f9df9be50346227c38d7a170fd3e8
leo <better_leo@163.com>
2021-04-16 13:14:56 +0800

    :star: 项目初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    title: "leo",
+    description: "保持一颗好奇的心，你会不断进步，变得更强！",
+    head: [
+        ["link", { rel: "icon", href: "/images/logo.jpg" }],
+        ["link", { rel: "manifest", href: "/images/logo.jpg" }],
+        ["link", { rel: "apple-touch-icon", href: "/images/logo.jpg" }]
+    ],
+    serviceWorker: true,
+    base: "/",
+    markdown: {
+        lineNumbers: false
+    },
+    themeConfig: {
+        logo: "/images/logo.jpg",
+        nav: [
+            { text: "首页", link: "/" },
+            { text: "学习笔记", link: "/note/" },
+            { text: "Github", link: "https://github.com/sjhleo" }
+        ],
+        sidebar: {
+            "/note/": [
+                {
+                    text: "笔记",
+                    collapsable: false,
+                    children: [
+                        { text: "概要", link: "/note/" },
+                        { text: "测试", link: "/note/test" }
+                    ]
+                }
+            ]
+        }
+
+        // sidebarDepth: 2
+    }
+};
